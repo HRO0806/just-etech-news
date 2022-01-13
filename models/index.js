7f13376f456e396609172775daa62e82ff3cee79
Henry <henryolson@icloud.com>
2022-01-13 17:46:38 -0600

    Forgot I was on the develop branch

--- models/index.js
+++ models/index.js
@@ -1,3 +1,4 @@
+const Vote = require('./Vote');
 const Post = require('./Post');
 const User = require('./User');
 
@@ -8,6 +9,17 @@ User.hasMany(Post, {
 
 Post.belongsTo(User, {
     foreignKey: 'user_id',
+});
+
+User.belongsToMany(Post, {
+    through: vote,
+    foreignKey: 'user_id'
+});
+
+Post.belongsToMany(User, {
+    through: Vote,
+    as: 'voted_posts',
+    foreignKey: 'post_id'
 });
 
 module.exports = { User, Post };
